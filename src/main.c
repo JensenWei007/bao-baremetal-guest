eb27b7b97f9497e0901f8f4018d89db54ecc6080
JensenWei007 <jensenwei007@gmail.com>
2026-04-08 08:14:16 +0000

    vm0
    Signed-off-by: JensenWei007 <jensenwei007@gmail.com>

--- src/main.c
+++ src/main.c
@@ -30,18 +30,18 @@
 spinlock_t print_lock = SPINLOCK_INITVAL;
 
 void uart_rx_handler(){
-    printf("cpu%d: %s\n",get_cpuid(), __func__);
+    printf("vm0: cpu%d: %s\n",get_cpuid(), __func__);
     uart_clear_rxirq();
 }
 
 void ipi_handler(){
     irq_clear_ipi();
-    printf("cpu%d: %s\n", get_cpuid(), __func__);
+    printf("vm0: cpu%d: %s\n", get_cpuid(), __func__);
     irq_send_ipi(1ull << (get_cpuid() + 1));
 }
 
 void timer_handler(){
-    printf("cpu%d: %s\n", get_cpuid(), __func__);
+    printf("vm0: cpu%d: %s\n", get_cpuid(), __func__);
     timer_set(TIMER_INTERVAL);
     irq_send_ipi(1ull << (get_cpuid() + 1));
 }
@@ -52,7 +52,7 @@ void main(void){
 
     if(cpu_is_master()){
         spin_lock(&print_lock);
-        printf("Bao bare-metal test guest\n");
+        printf("vm0: Bao bare-metal test guest\n");
         spin_unlock(&print_lock);
 
         irq_set_handler(UART_IRQ_ID, uart_rx_handler);
@@ -79,7 +79,7 @@ void main(void){
 
     while(!master_done);
     spin_lock(&print_lock);
-    printf("cpu %d up\n", get_cpuid());
+    printf("vm0: cpu %d up\n", get_cpuid());
     spin_unlock(&print_lock);
 
     while(1) wfi();
